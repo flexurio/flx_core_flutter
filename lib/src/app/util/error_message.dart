f8ce2b0b903373ab8fce83c592d9a2846b583a9a
Mualip Suhal <mualipsuhal@gmail.com>
2024-09-16 16:25:05 +0700

    ok

--- lib/src/app/util/error_message.dart
+++ lib/src/app/util/error_message.dart
@@ -1,8 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flexurio_erp_core/flexurio_erp_core.dart';
 
 String errorMessage(dynamic error) {
   if (error is ApiException) {
-    return error.message;
+    return 'error.${error.message}'.tr();
   } else {
     return errorSomethingWentWrong;
   }
