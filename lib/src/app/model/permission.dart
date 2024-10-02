1d2a1b0d0d6eecb2939cdc1fc4914e18e184631d
Mualip Suhal <mualipsuhal@gmail.com>
2024-10-02 13:43:25 +0700

    update permission

--- lib/src/app/model/permission.dart
+++ lib/src/app/model/permission.dart
@@ -221,18 +221,6 @@ class Permission {
       'pending_requests_recap_global_export_pdf';
 
   // --
-  static String scheduleViewMenu = 'schedule_view_menu';
-  static String scheduleCreate = 'schedule_create';
-  static String scheduleDelete = 'schedule_delete';
-  static String scheduleEdit = 'schedule_edit';
-  static String scheduleClose = 'schedule_close';
-  static String scheduleDetailCreate = 'schedule_detail_create';
-  static String scheduleDetailEdit = 'schedule_detail_edit';
-  static String scheduleDetailDelete = 'schedule_detail_delete';
-  static String scheduleDetailValidate = 'schedule_detail_validate';
-  static String scheduleExportExcel = 'schedule_export_excel';
-
-  // --
   static String supplierViewMenu = 'supplier_view_menu';
   static String supplierCreate = 'supplier_create';
   static String supplierDelete = 'supplier_delete';
@@ -385,59 +373,6 @@ class Permission {
       return DataAction.create;
     }
   }
-}
-
-class PermissionProduction {
-  // --
-  static String productionLineViewMenu = 'production_line_view_menu';
-  static String productionLineCreate = 'production_line_create';
-  static String productionLineDelete = 'production_line_delete';
-  static String productionLineEdit = 'production_line_edit';
-
-  // --
-  static String productionMachineViewMenu = 'production_machine_view_menu';
-  static String productionMachineCreate = 'production_machine_create';
-  static String productionMachineDelete = 'production_machine_delete';
-  static String productionMachineEdit = 'production_machine_edit';
-
-  // --
-  static String productionOrderViewMenu = 'production_order_view_menu';
-  static String productionOrderCreate = 'production_order_create';
-  static String productionOrderDelete = 'production_order_delete';
-  static String productionOrderEdit = 'production_order_edit';
-  static String productionOrderRework = 'production_order_rework';
-
-  // --
-  static String productionServiceOrderViewMenu =
-      'production_service_order_view_menu';
-  static String productionServiceOrderCreate =
-      'production_service_order_create';
-  static String productionServiceOrderDelete =
-      'production_service_order_delete';
-  static String productionServiceOrderEdit = 'production_service_order_edit';
-  static String productionServiceOrderRework =
-      'production_service_order_rework';
-
-  // --
-  static String productionStageViewMenu = 'production_stage_view_menu';
-  static String productionStageCreate = 'production_stage_create';
-  static String productionStageDelete = 'production_stage_delete';
-  static String productionStageEdit = 'production_stage_edit';
-  static String productionSubStageCreate = 'production_sub_stage_create';
-  static String productionSubStageDelete = 'production_sub_stage_delete';
-  static String productionSubStageEdit = 'production_sub_stage_edit';
-  static String productionSubStageActivate = 'production_sub_stage_activate';
-  static String productionSubStageDeactivate =
-      'production_sub_stage_deactivate';
-
-  // --
-  static String productionStageProcessViewMenu =
-      'production_stage_process_view_menu';
-  static String productionStageProcessCreate =
-      'production_stage_process_create';
-  static String productionStageProcessDelete =
-      'production_stage_process_delete';
-  static String productionStageProcessEdit = 'production_stage_process_edit';
 }
 
 class PermissionVendor {
