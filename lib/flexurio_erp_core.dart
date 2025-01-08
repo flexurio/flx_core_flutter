204d0a46a18655a0fe69d9331a7c0c9aab08306a
Mualip Suhal <mualipsuhal@gmail.com>
2025-01-08 13:08:50 +0700

    add drop_down_object

--- lib/flexurio_erp_core.dart
+++ lib/flexurio_erp_core.dart
@@ -79,3 +79,4 @@ export 'src/app/view/widget/time_line.dart';
 export 'src/app/view/widget/title_page.dart';
 export 'src/app/view/widget/top_bar.dart';
 export 'src/app/view/widget/yuhu_table.dart';
+export 'src/app/view/widget/drop_down_object.dart';
