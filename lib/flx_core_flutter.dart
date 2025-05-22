a652774f346e15c47c13408a6b1bb533ab89bd02
Mualip Suhal <mualipsuhal@gmail.com>
2025-05-22 14:24:07 +0700

    update core

--- lib/flx_core_flutter.dart
+++ lib/flx_core_flutter.dart
@@ -87,3 +87,4 @@ export 'src/app/view/widget/time_line.dart';
 export 'src/app/view/widget/title_page.dart';
 export 'src/app/view/widget/top_bar.dart';
 export 'src/app/view/widget/yuhu_table.dart';
+export 'src/app/util/storage_init.dart';
