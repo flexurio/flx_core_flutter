6c9174de60362e1a8d6ef92175e81ec0504a877d
Mualip Suhal <mualipsuhal@gmail.com>
2025-05-26 11:13:25 +0700

    fix entity not found message

--- lib/src/app/util/jwt.dart
+++ lib/src/app/util/jwt.dart
@@ -9,8 +9,3 @@ Map<String, dynamic> extractPayloadFromJwt(String jwt) {
   final payload = parts[1];
   return base64ToMap(payload);
 }
-
-Map<String, dynamic> base64ToMap(String text) {
-  final decodedPayload = base64Url.decode(base64.normalize(text.trim()));
-  return jsonDecode(utf8.decode(decodedPayload)) as Map<String, dynamic>;
-}
