45dec6f86dcdca52701a7aa4ef3da627b3d6a7ac
abdurrahman arifin <abdurrahman.arifin189@gmail.com>
2025-05-27 08:23:15 +0700

    add access button form vehicle rent

--- lib/src/app/model/permission.dart
+++ lib/src/app/model/permission.dart
@@ -257,6 +257,7 @@ class Permission {
   static String vehicleRentActualStartDate = 'vehicle_rent_actual_start_date';
   static String vehicleRentActualEndDate = 'vehicle_rent_actual_end_date';
   static String vehicleRentExportExcel = 'vehicle_rent_export_excel';
+  static String vehicleRentExportPdf = 'vehicle_rent_export_pdf';
 
   static List<String> toListString(List<String> permissions) {
     return permissions.map((e) => e).toList();
