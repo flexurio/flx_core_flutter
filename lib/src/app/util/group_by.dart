df6725d1fdd478b40f317a44b32b803dcf15b609
Mualip Suhal <mualipsuhal@gmail.com>
2025-06-05 12:01:52 +0700

    todo(suhal): REKAP DATA BIAYA DETAIL Accounting > Report > Cost Report > Export PDF Cost Report Detail — - Ditambahkan versi excel (format terlampir)

--- lib/src/app/util/group_by.dart
+++ lib/src/app/util/group_by.dart
@@ -12,31 +12,11 @@ Map<String, List<T>> groupBy<T>(List<T> items, String Function(T) group) {
   return data;
 }
 
-/// Returns the total sum of a numeric field from a list of objects.
-///
-/// This is a generic utility function that iterates over a list of type [T]
-/// and calculates the sum of values extracted by the provided [field] function.
-///
-/// Example:
-/// ```dart
-/// class Order {
-///   final int quantity;
-///   Order(this.quantity);
-/// }
-///
-/// final orders = [Order(2), Order(3), Order(5)];
-/// final totalQuantity = sum<Order>(orders, (o) => o.quantity); // 10
-/// ```
-///
-/// - [items]: The list of objects to sum.
-/// - [field]: A function that extracts the numeric value from each object.
-///
-/// Returns the sum as a [num], which may be an [int] or [double]
-/// depending on the values returned by [field].
-num sum<T>(List<T> items, num Function(T) field) => items.fold<num>(
-      0,
-      (sum, item) => sum + field(item),
-    );
+extension IterableSumExtension<T> on Iterable<T> {
+  num sum(num Function(T) selector) {
+    return map(selector).fold(0, (a, b) => a + b);
+  }
+}
 
 Map<String, T> groupByOne<T>(List<T> items, String Function(T) group) {
   final data = <String, T>{};
