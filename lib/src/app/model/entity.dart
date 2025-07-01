ac4217824f1e008556522bbbd385ae365d12f856
Mualip Suhal <mualipsuhal@gmail.com>
2025-07-01 09:36:20 +0700

    refactor(model): remove unused generalInvoice entity
    The generalInvoice entity was not being used anywhere in the codebase, so it was removed to clean up the model.
    
    #86czfy0g5

--- lib/src/app/model/entity.dart
+++ lib/src/app/model/entity.dart
@@ -84,11 +84,7 @@ class Entity extends EntityY {
         subtitleX: 'invoice_receive',
         iconPath: 'bill',
       );
-  static Entity get generalInvoice => const Entity(
-        titleX: 'general_invoice',
-        subtitleX: 'general_invoice',
-        iconPath: 'general_invoice',
-      );
+
   static Entity get invoiceReceiveDetail => const Entity(
         titleX: 'Invoice Receive Detail',
         subtitleX: '',
