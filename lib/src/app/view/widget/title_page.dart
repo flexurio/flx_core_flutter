5454cfe730a8cafaeb5466be9805de483565be24
Mualip Suhal <mualipsuhal@gmail.com>
2025-08-14 08:08:51 +0700

    improve style

--- lib/src/app/view/widget/title_page.dart
+++ lib/src/app/view/widget/title_page.dart
@@ -16,14 +16,57 @@ class TitlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.only(bottom: 24),
       child: Row(
         children: [
-          Image.asset(
-            'asset/image/icon/${entity.iconPath}.png',
+          Container(
             width: 64,
             height: 64,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              // linear-gradient(180deg,#e0f2fe 0,#f1f5f9 100%)
+              gradient: theme.modeCondition(
+                const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFFE0F2FE),
+                    Color(0xFFF1F5F9),
+                  ],
+                ),
+                LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    theme.cardColor,
+                    theme.cardColor,
+                  ],
+                ),
+              ),
+              // 0 10px 30px rgba(2,6,23,.07)
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0x12020617), // rgba(2,6,23,0.07)
+                  offset: Offset(0, 10),
+                  blurRadius: 30,
+                ),
+                BoxShadow(
+                  color: Color(0x08020617), // rgba(2,6,23,0.03)
+                  offset: Offset(0, 2),
+                  blurRadius: 6,
+                ),
+              ],
+            ),
+            child: Center(
+              child: Image.asset(
+                'asset/image/icon/${entity.iconPath}.png',
+                width: 48,
+                height: 48,
+              ),
+            ),
           ),
           const Gap(24),
           Expanded(
