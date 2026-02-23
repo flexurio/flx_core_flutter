9191454bac11821b3dd5312c179fcba07df8588e
Mualip Suhal <mualipsuhal@gmail.com>
2026-02-23 13:25:27 +0700

    feat: add Widgetbook stories for FDropDownSearchSmall widget.

--- lib/widgetbook.directories.g.dart
+++ lib/widgetbook.directories.g.dart
@@ -12,6 +12,8 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:flx_core_flutter/src/app/util/toast.widgetbook.dart'
     as _flx_core_flutter_src_app_util_toast_widgetbook;
+import 'package:flx_core_flutter/src/app/view/widget/f_drop_down/f_drop_down_search_small.widgetbook.dart'
+    as _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_search_small_widgetbook;
 import 'package:flx_core_flutter/src/app/view/widget/f_drop_down/f_drop_down_small.widgetbook.dart'
     as _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_small_widgetbook;
 import 'package:widgetbook/widgetbook.dart' as _widgetbook;
@@ -72,7 +74,36 @@ final directories = <_widgetbook.WidgetbookNode>[
                                 .dropDownSmallWithInitialValue,
                       ),
                     ],
-                  )
+                  ),
+                  _widgetbook.WidgetbookComponent(
+                    name: 'FDropDownSearchSmall',
+                    useCases: [
+                      _widgetbook.WidgetbookUseCase(
+                        name: 'Default',
+                        builder:
+                            _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_search_small_widgetbook
+                                .fDropDownSearchSmallDefault,
+                      ),
+                      _widgetbook.WidgetbookUseCase(
+                        name: 'Disabled',
+                        builder:
+                            _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_search_small_widgetbook
+                                .fDropDownSearchSmallDisabled,
+                      ),
+                      _widgetbook.WidgetbookUseCase(
+                        name: 'With Clear Button',
+                        builder:
+                            _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_search_small_widgetbook
+                                .fDropDownSearchSmallWithClearButton,
+                      ),
+                      _widgetbook.WidgetbookUseCase(
+                        name: 'With Initial Value',
+                        builder:
+                            _flx_core_flutter_src_app_view_widget_f_drop_down_f_drop_down_search_small_widgetbook
+                                .fDropDownSearchSmallWithInitialValue,
+                      ),
+                    ],
+                  ),
                 ],
               )
             ],
