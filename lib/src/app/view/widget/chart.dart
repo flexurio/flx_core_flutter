b4a47a11fbfc0b0e6daac683a52ed2259b2c438b
Mualip Suhal <mualipsuhal@gmail.com>
2026-04-22 16:43:44 +0700

    feat: rename DonutChart to FlxDonutChart and add FlxPieChart and FlxBarChart components

--- lib/src/app/view/widget/chart.dart
+++ lib/src/app/view/widget/chart.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flx_core_flutter/flx_core_flutter.dart';
+import 'f_card.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-class DonutChart extends StatelessWidget {
-  const DonutChart({required this.title, super.key, this.dataSource});
+class FlxDonutChart extends StatelessWidget {
+  const FlxDonutChart({required this.title, super.key, this.dataSource});
 
   final List<ChartData>? dataSource;
-
   final String title;
+
   @override
   Widget build(BuildContext context) {
     return FCard(
@@ -29,6 +29,72 @@ class DonutChart extends StatelessWidget {
               labelPosition: ChartDataLabelPosition.outside,
               useSeriesColor: true,
             ),
+            pointColorMapper: (data, _) => data.color,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FlxPieChart extends StatelessWidget {
+  const FlxPieChart({required this.title, super.key, this.dataSource});
+
+  final List<ChartData>? dataSource;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return FCard(
+      padding: const EdgeInsets.only(top: 12),
+      child: SfCircularChart(
+        title: ChartTitle(text: title),
+        legend: const Legend(
+          isVisible: true,
+          overflowMode: LegendItemOverflowMode.wrap,
+          position: LegendPosition.bottom,
+        ),
+        series: <PieSeries<ChartData, String>>[
+          PieSeries<ChartData, String>(
+            dataSource: dataSource,
+            xValueMapper: (data, _) => data.label,
+            yValueMapper: (data, _) => data.value,
+            dataLabelSettings: const DataLabelSettings(
+              isVisible: true,
+              labelPosition: ChartDataLabelPosition.outside,
+              useSeriesColor: true,
+            ),
+            pointColorMapper: (data, _) => data.color,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FlxBarChart extends StatelessWidget {
+  const FlxBarChart({required this.title, super.key, this.dataSource});
+
+  final List<ChartData>? dataSource;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return FCard(
+      padding: const EdgeInsets.only(top: 12),
+      child: SfCartesianChart(
+        title: ChartTitle(text: title),
+        primaryXAxis: const CategoryAxis(),
+        legend: const Legend(
+          isVisible: true,
+          position: LegendPosition.bottom,
+        ),
+        series: <CartesianSeries<ChartData, String>>[
+          ColumnSeries<ChartData, String>(
+            dataSource: dataSource,
+            xValueMapper: (data, _) => data.label,
+            yValueMapper: (data, _) => data.value,
+            dataLabelSettings: const DataLabelSettings(isVisible: true),
             pointColorMapper: (data, _) => data.color,
           ),
         ],
